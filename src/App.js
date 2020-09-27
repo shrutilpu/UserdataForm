edfc79d488f4de57df227a31e0a647e11f1544a0
shrutilpu <shr1999uti@gmail.com>
2020-09-27 22:29:33 +0530

    form created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useForm} from 'react-hook-form';
+import axios from 'axios';
 
 function App() {
+  const {register,handleSubmit,errors} = useForm();
+
+  const onSubmitHandler =(data)=>{
+     axios.post("https://userdata-4e250.firebaseio.com/data.json",data).then(res=>{
+       onclose();
+       return alert("Saved successfully");
+     }).catch(e=>console.log(e.message));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <form name="UserData" className="Form" onSubmit={handleSubmit(onSubmitHandler)}>
+      <h3>USER DATA</h3>
+       <input type="text" placeholder="Name" name="Name" ref={register({required:true})}/>
+       {errors.Name && <p>please enter your name</p>}
+
+       <input type="email" placeholder="E-mail" name="Email" ref={register({required:true})}/>
+       {errors.Email && <p>please enter valid email id</p>}
+
+       <input type="number" placeholder="phone no." name="phone" ref={register({required:true, maxLength:10 ,minLength:10})}/>
+       {errors.phone && <p>please enter correct mobile number</p>}
+
+     <div className="Radiobtn">
+       <label>Gender:</label>
+        <input type="radio" value="Male" name="gender" ref={register({required:true})}/> Male
+        <input type="radio" value="Female" name="gender" ref={register({required:true})}/> Female
+        <input type="radio" value="Other" name="gender" ref={register({required:true})}/> Other
+     </div>
+     {errors.gender && <p>choose your gender</p>}
+     <div className="Skills">
+       <label>Skills:</label>
+     <select name="skills" ref={register({required:true})}>
+         <option value="UI">UI</option>
+         <option value="Ux">UX</option>
+         <option value="Backend">Backend</option>
+         <option value="CSS">CSS</option>
+       </select>
+     </div>
+       <textarea name="Description" placeholder="Description" className="Text" ref={register}/>
+       <div className="btn">
+         <button type="reset">Edit</button>
+       <button type="submit">save</button>
+       </div>
+       
+     </form>
     </div>
   );
 }
